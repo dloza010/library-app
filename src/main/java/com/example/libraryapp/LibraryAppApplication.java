8ffd021edc66f13b135ee250be0260a3739c3357
David Lozano <david.loza010@gmail.com>
2023-09-25 16:23:55 -0400

    database connection config

--- src/main/java/com/example/libraryapp/LibraryAppApplication.java
+++ src/main/java/com/example/libraryapp/LibraryAppApplication.java
@@ -1,17 +1,34 @@
 package com.example.libraryapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
 public class LibraryAppApplication {
 
     public static void main(String[] args) {
+        applicationConfig();
         SpringApplication.run(LibraryAppApplication.class, args);
+    }
+
+    protected static void applicationConfig() {
+        Dotenv dotenv = Dotenv.configure().load();
+
+        // Getting configuration from .env file
+        String dataSourceUrl = dotenv.get("DATASOURCE_URL");
+        String dataSourceUsername = dotenv.get("DATASOURCE_USERNAME");
+        String datasourcePassword = dotenv.get("DATASOURCE_PASSWORD");
+
+        // Setting application.property configuration from .env file key
+        System.setProperty("spring.datasource.url", dataSourceUrl);
+        System.setProperty("spring.datasource.username", dataSourceUsername);
+        System.setProperty("spring.datasource.password", datasourcePassword);
     }
 
     @GetMapping("/")
