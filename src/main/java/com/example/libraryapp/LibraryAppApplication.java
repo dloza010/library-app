de1f2b623a540b0cad4464f3831f38f00487436d
David Lozano <david.loza010@gmail.com>
2023-09-30 19:53:39 -0400

    Removed database configuration logic from main SpringBootApplication file and stored it in bootstrap folder

--- src/main/java/com/example/libraryapp/LibraryAppApplication.java
+++ src/main/java/com/example/libraryapp/LibraryAppApplication.java
@@ -1,12 +1,11 @@
 package com.example.libraryapp;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import static com.example.libraryapp.bootstrap.Config.applicationConfig;
 
 @SpringBootApplication
 @RestController
@@ -16,21 +15,6 @@ public class LibraryAppApplication {
         applicationConfig();
         SpringApplication.run(LibraryAppApplication.class, args);
     }
-
-    protected static void applicationConfig() {
-        Dotenv dotenv = Dotenv.configure().load();
-
-        // Getting configuration from .env file
-        String dataSourceUrl = dotenv.get("DATASOURCE_URL");
-        String dataSourceUsername = dotenv.get("DATASOURCE_USERNAME");
-        String datasourcePassword = dotenv.get("DATASOURCE_PASSWORD");
-
-        // Setting application.property configuration from .env file key
-        System.setProperty("spring.datasource.url", dataSourceUrl);
-        System.setProperty("spring.datasource.username", dataSourceUsername);
-        System.setProperty("spring.datasource.password", datasourcePassword);
-    }
-
     @GetMapping("/")
     public String hello(){
         return "Homepage!";
